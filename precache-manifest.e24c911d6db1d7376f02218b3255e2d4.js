3d2403d39d7242d86986f1d9eaad96df8b6cd049
MedynskyyOstap <47416022+voloxatyj@users.noreply.github.com>
2020-02-07 19:45:16 +0200

    Updates

---
+++ precache-manifest.e24c911d6db1d7376f02218b3255e2d4.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = [
+  {
+    "revision": "82cea312357e40f635ab",
+    "url": "/GameZone/static/js/app.23058c9b.chunk.js"
+  },
+  {
+    "revision": "4d97e4a00c07d5db045e",
+    "url": "/GameZone/static/js/runtime~app.1733c83f.js"
+  },
+  {
+    "revision": "604b0af0c3d258d8ab5e",
+    "url": "/GameZone/static/js/2.611764b2.chunk.js"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/GameZone/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "1331d140a3066b04261c2342fbf10eb6",
+    "url": "/GameZone/static/media/game_bg.1331d140.png"
+  },
+  {
+    "revision": "87254a16bb34a30d7ba8fd3d2649724b",
+    "url": "/GameZone/static/media/heart_logo.87254a16.png"
+  },
+  {
+    "revision": "d2e691bc4a2b696929172cb3d22ce8ba",
+    "url": "/GameZone/./fonts/Nunito-Regular.ttf"
+  },
+  {
+    "revision": "1cd294a771f26752bbb8d8d5210f6412",
+    "url": "/GameZone/./fonts/Nunito-Bold.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/GameZone/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/GameZone/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/GameZone/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/GameZone/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/GameZone/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/GameZone/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/GameZone/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/GameZone/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/GameZone/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/GameZone/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/GameZone/serve.json"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/GameZone/favicon.ico"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/GameZone/expo-service-worker.js"
+  },
+  {
+    "revision": "af42f7bb032bbaa6050c442c9a85646e",
+    "url": "/GameZone/manifest.json"
+  },
+  {
+    "revision": "ef58ff9a14b1871e5232fada458806e5",
+    "url": "/GameZone/apple/icons/icon_180x180.png"
+  },
+  {
+    "revision": "dd0d8974f196283ab21f6bb647882750",
+    "url": "/GameZone/apple/icons/icon_192x192.png"
+  },
+  {
+    "revision": "b370d9dab464db3e849d854262b5d2e5",
+    "url": "/GameZone/apple/icons/icon_512x512.png"
+  },
+  {
+    "revision": "b4c95fe40cc182ee41709b5386a65a60",
+    "url": "/GameZone/apple/splash/icon_1125x2436.png"
+  },
+  {
+    "revision": "24057a53b4b8207ac6128f5a1858a41f",
+    "url": "/GameZone/apple/splash/icon_1136x640.png"
+  },
+  {
+    "revision": "d8bfb76284287658cab36f271d2a2a4a",
+    "url": "/GameZone/apple/splash/icon_1242x2208.png"
+  },
+  {
+    "revision": "46f33d43a6ee160cfb767b7e565d3963",
+    "url": "/GameZone/apple/splash/icon_1242x2688.png"
+  },
+  {
+    "revision": "ff41747f4f3bb2f9a29c2ee494815852",
+    "url": "/GameZone/apple/splash/icon_1334x750.png"
+  },
+  {
+    "revision": "79a7a4869dc12a14e286c382d496dfd0",
+    "url": "/GameZone/apple/splash/icon_1792x828.png"
+  },
+  {
+    "revision": "7e9fc39b1c37a328126323bbe8cd404b",
+    "url": "/GameZone/apple/splash/icon_2208x1242.png"
+  },
+  {
+    "revision": "1fd86c39fec0a11f2f6b61d5a7af0f39",
+    "url": "/GameZone/apple/splash/icon_2436x1125.png"
+  },
+  {
+    "revision": "dbbf9209e64340c9d1b78823b8f8abf2",
+    "url": "/GameZone/apple/splash/icon_2688x1242.png"
+  },
+  {
+    "revision": "df18c0c33e95b50b62f822d65cd9b909",
+    "url": "/GameZone/apple/splash/icon_640x1136.png"
+  },
+  {
+    "revision": "5d0bcf1c916780f7a20f047bb6d8a30c",
+    "url": "/GameZone/apple/splash/icon_750x1334.png"
+  },
+  {
+    "revision": "4ed2cf28de535595662c61587b07aa56",
+    "url": "/GameZone/apple/splash/icon_828x1792.png"
+  },
+  {
+    "revision": "1603e4da552bf0d3941747a69874fefd",
+    "url": "/GameZone/index.html"
+  },
+  {
+    "revision": "7cdd85983bee54b64aeb6aad4a9c6cf5",
+    "url": "/GameZone/static/js/app.23058c9b.chunk.js.gz"
+  },
+  {
+    "revision": "195ec855db09aff01d81aaf480bafd8e",
+    "url": "/GameZone/static/js/runtime~app.1733c83f.js.gz"
+  },
+  {
+    "revision": "4a2a8019995a0671e46de9607f5dfe6c",
+    "url": "/GameZone/expo-service-worker.js.gz"
+  },
+  {
+    "revision": "5c8fa89693b23492ac38e75a01edc999",
+    "url": "/GameZone/static/js/2.611764b2.chunk.js.gz"
+  }
+];
